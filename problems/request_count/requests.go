d500c108dc355dfe88d2f52374bc71754aad5413
Matt Weiden <538456+mweiden@users.noreply.github.com>
2017-11-11 14:26:21 -0800

    Refactor for better style

--- problems/request_count/requests.go
+++ problems/request_count/requests.go
@@ -12,12 +12,12 @@ type RequestCount struct {
 
 type RequestMetrics struct {
 	getTimestamp    func() int64
-	interval        int64
+	interval        uint32
 	timestampCounts []RequestCount
 	counterMutex    sync.RWMutex
 }
 
-func NewRequestMetrics(interval int64, getTimestamp func() int64) RequestMetrics {
+func NewRequestMetrics(interval uint32, getTimestamp func() int64) RequestMetrics {
 	if interval < 0 {
 		panic(1)
 	}
@@ -40,37 +40,41 @@ func (r *RequestMetrics) sanityCheckTimestamp(timestamp int64) {
 }
 
 func (r *RequestMetrics) Inc(amount uint) {
-	if amount != 0 {
-		r.counterMutex.Lock()
-		// if storage is growing too large, garbage collect
-		if int64(len(r.timestampCounts)) >= r.interval {
-			r.garbageCollect()
-		}
-		timestamp := r.getTimestamp()
-		r.sanityCheckTimestamp(timestamp)
-		lastInd := len(r.timestampCounts) - 1
-		overlap := lastInd >= 0 &&
-			r.timestampCounts[lastInd].timestamp == timestamp
-		if !overlap {
-			r.timestampCounts = append(
-				r.timestampCounts,
-				RequestCount{timestamp, 1},
-			)
-		} else {
-			r.timestampCounts[lastInd].count++
-		}
-		r.counterMutex.Unlock()
+	if amount == 0 {
+		return
 	}
+	timestamp := r.getTimestamp()
+	r.sanityCheckTimestamp(timestamp)
+
+	// if storage is growing too large, garbage collect
+	if uint32(len(r.timestampCounts)) > r.interval {
+		r.garbageCollect()
+	}
+
+	// ok, actually increment
+	r.counterMutex.Lock()
+	lastInd := len(r.timestampCounts) - 1
+	overlap := lastInd >= 0 &&
+		r.timestampCounts[lastInd].timestamp == timestamp
+	if !overlap {
+		r.timestampCounts = append(
+			r.timestampCounts,
+			RequestCount{timestamp, 1},
+		)
+	} else {
+		r.timestampCounts[lastInd].count++
+	}
+	r.counterMutex.Unlock()
 }
 
 func (r *RequestMetrics) Count() uint64 {
 	r.counterMutex.RLock()
 	end := r.getTimestamp()
 	var start int64
-	if r.interval > end {
+	if int64(r.interval) > end {
 		start = 0
 	} else {
-		start = end - r.interval
+		start = end - int64(r.interval)
 	}
 	nTimestamps := len(r.timestampCounts)
 	i := 0
@@ -102,13 +106,15 @@ func (r *RequestMetrics) Count() uint64 {
 func (r *RequestMetrics) garbageCollect() {
 	timestamp := r.getTimestamp()
 	r.sanityCheckTimestamp(timestamp)
-	start := timestamp - r.interval
+	r.counterMutex.Lock()
+	start := timestamp - int64(r.interval)
 	// remove the timestamp counts before start, possible cases are:
 	// 1. if start is less than the first timestamp in storage, slice from the first timestamp
 	// 2. if the timestamp is found, slice from the next one
 	// 3. if a lesser timestamp is found, slice from the next one
 	i := binarySearch(start, len(r.timestampCounts), r.timestampAt) + 1
 	r.timestampCounts = r.timestampCounts[i:len(r.timestampCounts)]
+	r.counterMutex.Unlock()
 }
 
 func (r *RequestMetrics) timestampAt(i int) int64 {
